70319b61ce78fb8952b89a770669e50b3f70e212
Thanin Tangkoblap <tangkoblap_t@su.ac.th>
2025-09-20 14:16:28 +0000

    Commit : Sent class exercise ( Bookstore101 ).

---
+++ my-app-bookstore/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './HomePage.css'; // ✨ Import from style folder
+// import SearchBar from '../components/SearchBar';
+// import BookCard from '../components/BookCard';
+// import { getFeaturedBooks, getCategories } from '../data/booksData';
+
+const HomePage = () => {
+
+  return (
+    <div className="home-page">
+      {/* Hero Section */}
+      <h1>Hello Labboy.</h1>
+      <p>My name is Boss thanin tangkoblap 660710714</p>
+    </div>
+  );
+};
+
+export default HomePage;
